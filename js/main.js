311e2626ec8ee04965ea50eb156e85df08ff6cad
MohamedEmadEldaly <mohamedemad201194@gmail.com>
2018-04-23 21:18:33 +0200

    init medix

---
+++ js/main.js
@@ -0,0 +1,38 @@
+/*global console,$,jquery*/ 
+$(document).ready(function (){
+
+    'use strict';
+
+    // navigation button active
+
+    $('.navbar-toggle').on("click", function () {
+        $(this).toggleClass('active');
+    });
+
+    // close nav when click some where else 
+    
+
+    //revoluation slider
+    $(document).ready(function() {
+        $('.tp-banner').revolution(
+         {
+             delay:9000,
+             startwidth:1170,
+             startheight:500,
+             hideThumbs:10,
+             fullWidth:"on",
+             onHoverStop:"off",
+             navigationStyle:"round",
+             navigationHAlign:"center"
+         });
+    });
+
+    
+
+    console.log($(window).height() - $('header').height());
+    
+
+
+
+
+});
